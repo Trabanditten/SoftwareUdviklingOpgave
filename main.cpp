268d71d971664389d879054a94bfb923225fd433
Andreas <andrp24@student.sdu.dk>
2025-03-06 14:09:54 +0100

    usercase2+3

--- main.cpp
+++ main.cpp
@@ -4,24 +4,47 @@
 
 class Task {
 public:
-    std::string _description;
-    Task(std::string description) : _description(description) {}
+    std::string description;
+    bool completed;
+
+    Task(std::string desc) : description(desc), completed(false) {}
+    void markCompleted() { completed = true; }
 };
 
 class TodoList {
 public:
-    std::vector<Task> todolist;
-    void addTask(std::string desc) { todolist.push_back(Task(desc)); }
+    std::vector<Task> tasks;
+
+    void addTask(const std::string& desc) { tasks.push_back(Task(desc)); }
+    void markTaskCompleted(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks[index].markCompleted();
+        }
+    }
+    void displayTasks() const {
+        for (size_t i = 0; i < tasks.size(); ++i) {
+            std::cout << i + 1 << ". " << tasks[i].description << " [" << (tasks[i].completed ? "✔" : "✘") << "]\n";
+        }
+    }
+    void viewAllTasks() const {
+        std::cout << "Alle opgaver:\n";
+        displayTasks();
+    }
 };
 
 int main() {
     TodoList todo;
-    std::string task;
+    todo.addTask("Køb ind");
+    todo.addTask("Lær C++");
     
-    std::cout << "Tilføj en opgave: ";
-    std::getline(std::cin, task);
-    todo.addTask(task);
-
+    todo.viewAllTasks();
+    
+    int taskNumber;
+    std::cout << "Markér en opgave som fuldført (indtast nummer): ";
+    std::cin >> taskNumber;
+    todo.markTaskCompleted(taskNumber - 1);
+    
+    todo.viewAllTasks();
     return 0;
 }
 
