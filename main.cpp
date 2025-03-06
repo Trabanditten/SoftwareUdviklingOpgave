562b8a3195937646c59d095d095975c2ccc6bb05
Dania albunni <daalb24@student.sdu.dk>
2025-03-06 13:41:48 +0100

    usercase1

--- main.cpp
+++ main.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
+#include <string>
 
-
-
-
-
-
-
-class Task
-{
-	public:
-	std::string _description;
-	Task(std::string description){
-		_description = description;
-	}
+class Task {
+public:
+    std::string _description;
+    Task(std::string description) : _description(description) {}
 };
 
-class TodoList
-{
-	
-	public:
-	std::vector<Task> todolist;
-	TodoList(){}
+class TodoList {
+public:
+    std::vector<Task> todolist;
+    void addTask(std::string desc) { todolist.push_back(Task(desc)); }
 };
 
 int main() {
-
-    TodoList todo = TodoList();
+    TodoList todo;
+    std::string task;
+    
+    std::cout << "Tilføj en opgave: ";
+    std::getline(std::cin, task);
+    todo.addTask(task);
 
     return 0;
 }
+
