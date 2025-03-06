093bd5eacb8167cd2924dfb88f855d7f93f621bc
Mads Trabandt <Mtrab24@student.sdu.dk>
2025-03-06 14:28:02 +0100

    Usercase 4+5

--- main.cpp
+++ main.cpp
@@ -7,28 +7,39 @@ public:
     std::string description;
     bool completed;
 
-    Task(std::string desc) : description(desc), completed(false) {}
+    Task(const std::string& desc) : description(desc), completed(false) {}
     void markCompleted() { completed = true; }
+    void editDescription(const std::string& newDesc) { description = newDesc; }
 };
 
 class TodoList {
 public:
     std::vector<Task> tasks;
 
-    void addTask(const std::string& desc) { tasks.push_back(Task(desc)); }
+    void addTask(const std::string& desc) { tasks.emplace_back(desc); }
+
     void markTaskCompleted(int index) {
         if (index >= 0 && index < tasks.size()) {
             tasks[index].markCompleted();
         }
     }
-    void displayTasks() const {
+
+    void editTask(int index, const std::string& newDesc) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks[index].editDescription(newDesc);
+        }
+    }
+
+    void deleteTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.erase(tasks.begin() + index);
+        }
+    }
+
+    void viewAllTasks() const {
         for (size_t i = 0; i < tasks.size(); ++i) {
             std::cout << i + 1 << ". " << tasks[i].description << " [" << (tasks[i].completed ? "✔" : "✘") << "]\n";
         }
-    }
-    void viewAllTasks() const {
-        std::cout << "Alle opgaver:\n";
-        displayTasks();
     }
 };
 
@@ -36,15 +47,42 @@ int main() {
     TodoList todo;
     todo.addTask("Køb ind");
     todo.addTask("Lær C++");
-    
-    todo.viewAllTasks();
-    
-    int taskNumber;
-    std::cout << "Markér en opgave som fuldført (indtast nummer): ";
-    std::cin >> taskNumber;
-    todo.markTaskCompleted(taskNumber - 1);
-    
-    todo.viewAllTasks();
+
+    int choice, taskNumber;
+    std::string newDesc;
+    do {
+        std::cout << "\n1. Markér opgave som fuldført\n";
+        std::cout << "2. Slet opgave\n";
+        std::cout << "3. Rediger opgave\n";
+        std::cout << "4. Vis alle opgaver\n";
+        std::cout << "5. Afslut\n";
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1:
+            std::cout << "Indtast nummer på opgaven at markere som fuldført: ";
+            std::cin >> taskNumber;
+            todo.markTaskCompleted(taskNumber - 1);
+            break;
+        case 2:
+            std::cout << "Indtast nummer på opgaven at slette: ";
+            std::cin >> taskNumber;
+            todo.deleteTask(taskNumber - 1);
+            break;
+        case 3:
+            std::cout << "Indtast nummer på opgaven at redigere: ";
+            std::cin >> taskNumber;
+            std::cin.ignore(); 
+            std::cout << "Indtast ny beskrivelse: ";
+            std::getline(std::cin, newDesc);
+            todo.editTask(taskNumber - 1, newDesc);
+            break;
+        case 4:
+            todo.viewAllTasks();
+            break;
+        }
+    } while (choice != 5);
+
     return 0;
 }
 
